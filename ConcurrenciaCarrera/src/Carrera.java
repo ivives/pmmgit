f295bfd5b5b99439005640cef6a76a24ee12ad0f
ivives <ivives75@gmail.com>
2013-11-12 21:46:24 +0100

    termino concurrencia carrera

--- ConcurrenciaCarrera/src/Carrera.java
+++ ConcurrenciaCarrera/src/Carrera.java
@@ -6,14 +6,14 @@ public class Carrera {
 	 */
 	public static void main(String[] args) {
 		TortugaThread tortuga = new TortugaThread();
-		LiebreThread liebre = new Thread(new LiebreThread()); // LiebreThread implements Runnable
+		LiebreThread liebre = new LiebreThread(); // LiebreThread implements Runnable
 		System.out.println(tortuga.isAlive());
 		System.out.println(liebre.isAlive());
 		
 		tortuga.start();
 		liebre.start();
-		System.out.println("Â¿Ha comenzado Tortuga?: " + tortuga.isAlive());
-		System.out.println("Â¿Ha comenzado Liebre?: " + liebre.isAlive());
+		System.out.println("¿Ha comenzado Tortuga?: " + tortuga.isAlive());
+		System.out.println("¿Ha comenzado Liebre?: " + liebre.isAlive());
 		
 
 	}
---
+++ ConcurrenciaCarrera/src/LiebreThread.java
@@ -0,0 +1,19 @@
+
+public class LiebreThread  extends Thread{
+	public void run(){
+		int i = 0;
+		System.out.println("Comienza la liebre.");
+		while(i<5){
+			try{
+				Thread.sleep(1000);
+				System.out.println("Liebre.");
+			}
+			catch(InterruptedException ex){
+			
+			}
+			i++;
+		}
+		System.out.println("Termina la liebre.");
+	}
+
+}
