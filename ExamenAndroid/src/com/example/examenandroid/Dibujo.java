adb3271bb4ab8019f9eb1e25932f43fa605ee5a9
ivives <ivives75@gmail.com>
2014-01-07 21:57:53 +0100

    cambio api a examen

--- ExamenAndroid/src/com/example/examenandroid/Dibujo.java
+++ ExamenAndroid/src/com/example/examenandroid/Dibujo.java
@@ -114,11 +114,5 @@ public class Dibujo extends Activity {
 		}
 	}
 	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
+	
 }
