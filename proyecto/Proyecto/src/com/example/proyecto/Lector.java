32cc7269bf61d1998a486a36a4c7b16e5d15278b
ivives <ivives75@gmail.com>
2014-04-26 23:54:30 +0200

    consigo unir las dos partes ahora escaneo los codigos, los busco en la DB y muestro los datos

--- proyecto/Proyecto/src/com/example/proyecto/Lector.java
+++ proyecto/Proyecto/src/com/example/proyecto/Lector.java
@@ -1,16 +1,34 @@
 package com.example.proyecto;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -19,19 +37,25 @@ import android.widget.Toast;
 
 public class Lector extends Activity {
 
-	/** Se le llama cuando se crea por primera vez la actividad. */
+	//Se le llama cuando se crea por primera vez la actividad
 	  private static final String BS_PACKAGE = "com.google.zxing.client.android";
 	  public static final int REQUEST_CODE = 0x0000c0de;
-	  private TextView lbl;
 	  private Button boton;
 	  private Activity activity;
+	  private String contents;
+	  private TextView codigo;
+	  private TextView descripcion;
+	  private TextView precio;
 @Override
 public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     activity=this;
     setContentView(R.layout.activity_lector);
-	    lbl=(TextView) findViewById(R.id.lblCodigo);	    
+	       
 	    boton=(Button)findViewById(R.id.button1);
+	    codigo = (TextView) findViewById(R.id.textoCodigo);
+		descripcion = (TextView) findViewById(R.id.textoDescripcion);
+		precio = (TextView) findViewById(R.id.textoPrecio);
 	    
 	    boton.setOnClickListener(new OnClickListener() {
 			
@@ -90,10 +114,10 @@ private AlertDialog showDownloadDialog() {
     return downloadDialog.show();
   }
 
-	  public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+	  public void onActivityResult(int requestCode, int resultCode, Intent intent ) {
 		    if (requestCode == REQUEST_CODE) {
 		        if (resultCode == Activity.RESULT_OK) {
-		          String contents = intent.getStringExtra("SCAN_RESULT");
+		          contents = intent.getStringExtra("SCAN_RESULT");
 		          String formatName = intent.getStringExtra("SCAN_RESULT_FORMAT");
 		          byte[] rawBytes = intent.getByteArrayExtra("SCAN_RESULT_BYTES");
 		          int intentOrientation = intent.getIntExtra("SCAN_RESULT_ORIENTATION", Integer.MIN_VALUE);
@@ -101,9 +125,89 @@ private AlertDialog showDownloadDialog() {
 		          String errorCorrectionLevel = intent.getStringExtra("SCAN_RESULT_ERROR_CORRECTION_LEVEL");
 		          Toast.makeText(this, contents, Toast.LENGTH_LONG).show();
 
-		          lbl.setText(contents);
+		          	  		
+					//Obtenemos los datos
+					DoPOST mDoPOST = new DoPOST(Lector.this, contents);
+					mDoPOST.execute("");		
+		          
 		        }
 		      }
+		      		      
 		      return ;
 	  }
+	  
+	  public class DoPOST extends AsyncTask<String, Void, Boolean>{
+
+			Context mContext = null;
+			String strCodeToSearch = "";
+			
+			//Los datos resultantes
+			String strCodigo;
+			String strDescripcion;
+			double douPrecio;
+			
+			
+			Exception exception = null;
+			
+			DoPOST(Context context, String codeToSearch){
+				mContext = context;
+				strCodeToSearch = codeToSearch;
+			}
+
+			@Override
+			protected Boolean doInBackground(String... arg0) {
+
+				try{
+
+					//Configuracion de los parametros
+					ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+					nameValuePairs.add(new BasicNameValuePair("CodeToSearch", strCodeToSearch));	
+					
+					//Creacion de la peticion HTTP
+					HttpParams httpParameters = new BasicHttpParams();
+
+					//Tiempos de espera para la peticion
+					HttpConnectionParams.setConnectionTimeout(httpParameters, 15000);
+					HttpConnectionParams.setSoTimeout(httpParameters, 15000);			
+
+					HttpClient httpclient = new DefaultHttpClient(httpParameters);
+					HttpPost httppost = new HttpPost("http://192.168.1.103/clienteservidor/login.php");
+					httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));        
+					HttpResponse response = httpclient.execute(httppost);
+					HttpEntity entity = response.getEntity();
+
+					String result = EntityUtils.toString(entity);
+
+					// Creamos un objeto JSON con la respuesta de la peticion
+					JSONObject jsonObject = new JSONObject(result);
+
+					//Recuperamos los datos del objeto JSON
+					strCodigo = jsonObject.getString("codigo");
+					strDescripcion = jsonObject.getString("descripcion");
+					douPrecio = jsonObject.getDouble("precio");
+					
+
+				}catch (Exception e){
+					Log.e("ClienteServidor", "Error:", e);
+					exception = e;
+				}
+
+				return true;
+			}
+
+			@Override
+			protected void onPostExecute(Boolean valid){
+				//Actualizamos la interfaz de usuario
+				
+				codigo.setText("Codigo: " + strCodigo);
+				descripcion.setText("Descripcion: " + strDescripcion);
+				precio.setText("Precio: " + douPrecio);	
+								
+				if(exception != null){
+					Toast.makeText(mContext, exception.getMessage(), Toast.LENGTH_LONG).show();
+				}
+			}
+
+		}
+	  	  
 }
